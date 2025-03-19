0700fbba8c02428317bd286eb03dec66961393e3
Toshiyuki SHIMONO <t.shimono@yamanashi.ac.jp>
2025-03-19 17:21:05 +0900

    5個のファイル : モジュール化するために 追加格納。

---
+++ bookletps.pm
@@ -0,0 +1,25 @@
+package App::bookletps ;  
+our $VERSION = '0.010' ; 
+our $DATE = '2025-03-18T10:07+09:00' ; 
+
+=encoding utf8
+
+=head1 NAME
+
+App::bookletps - Adobe等のソフトで小冊子印刷のページ指定時に、1枚印刷するごとに元のPDFから順に4ページずつになるようにする。
+
+=head1 SYNOPSIS
+
+This module provides a Unix-like command `F<bookletps>'. 
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+bookletps --help
+man bookletps
+perldoc bookletps
+
+=cut
+
+1 ;
