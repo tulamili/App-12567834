f0acaa07f828879cae7c360f6c8e8e2a5d9c4130
Toshiyuki SHIMONO <t.shimono@yamanashi.ac.jp>
2025-04-01 16:45:31 +0900

    7個のファイルについて、bookletps を 12567834 に変更。

--- 12567834.pm
+++ 12567834.pm
@@ -1,24 +1,24 @@
-package App::bookletps ;  
-our $VERSION = '0.010' ; 
-our $DATE = '2025-03-18T10:07+09:00' ; 
+package App::12567834 ;  
+our $VERSION = '0.020' ; 
+our $DATE = '2025-04-01T16:40+09:00' ; 
 
 =encoding utf8
 
 =head1 NAME
 
-App::bookletps - Adobe等のソフトで小冊子印刷のページ指定時に、1枚印刷するごとに元のPDFから順に4ページずつになるようにする。
+App::12567834 - Adobe等のソフトで小冊子印刷のページ指定時に、1枚印刷するごとに元のPDFから順に4ページずつになるようにする。
 
 =head1 SYNOPSIS
 
-This module provides a Unix-like command `F<bookletps>'. 
+This module provides a Unix-like command `F<12567834>'. 
 
 =head1 DESCRIPTION
 
 =head1 SEE ALSO
 
-bookletps --help
-man bookletps
-perldoc bookletps
+12567834 --help
+man 12567834
+perldoc 12567834
 
 =cut
 
